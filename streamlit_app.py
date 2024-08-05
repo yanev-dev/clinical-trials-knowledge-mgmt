403a104304199ebf6cb74c118633cf00f7c7a099
yanev-dev <127370286+yanev-dev@users.noreply.github.com>
2024-08-04 23:30:18 -0400

    Update streamlit_app.py
    docs fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ text_splitter = SemanticChunker(
 )   
 
 uploaded_docs = st.file_uploader('Upload trial documents in PDF format.',
-                                   type=["pdf",],
-                                   accept_multiple_files=True)
+                                 type=["pdf",],
+                                 accept_multiple_files=True)
 
 system_prompt = (
         """
@@ -62,40 +62,38 @@ prompt = ChatPromptTemplate.from_messages(
 
 splits = []
 
-with st.spinner('Retrieving...'):
+if uploaded_docs is not None:
+    # parse the docs
     for f in uploaded_docs:
         loader = PyPDFLoader(os.path.join("/tmp", file))
         docs = loader.load()
-
         # split the documents into chunks
         splits.extend(text_splitter.split_documents(docs))
 
-    if uploaded_docs is not None:
-        # create the vectorestore to use as the index
-        vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-        # expose this index in a retriever interface
-        retriever = vectorstore.as_retriever()
-        # create a chain to answer questions 
-        question_answer_chain = create_stuff_documents_chain(llm, prompt)
-        rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    # create the vectorestore to use as the index
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+    # expose this index in a retriever interface
+    retriever = vectorstore.as_retriever()
+    # create a chain to answer questions 
+    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
 
-        # Ask the user for a question via `st.text_area`.
-        question = st.text_area(
-            "Now ask a question about the documents!",
-            placeholder="Can you give me a short summary of the protocol?",
-            disabled=not uploaded_docs,
-        )
+    # ask the user for a question via `st.text_area`.
+    question = st.text_area(
+        "Now ask a question about the documents!",
+        placeholder="Can you give me a short summary of the protocol?",
+        disabled=not uploaded_docs,
+    )
 
-
-        with st.form(key="questions"):
-            question = st.text_input('Question to answer:')
-            retrieve = st.form_submit_button("Ask", type="primary")
-            if retrieve:
-                results = rag_chain.invoke({"input": question})
-                if results:
-                    st.write((results['answer']))
-                    st.write("Sources:")
-                    for idx, item in enumerate(results['context']):
-                        st.write('\nSource %s:' % str(idx+1))
-                        st.write(results['context'][idx].metadata)
-                        st.write(results['context'][idx].page_content)
+    with st.form(key="questions"):
+        question = st.text_input('Question to answer:')
+        retrieve = st.form_submit_button("Ask", type="primary")
+        if retrieve:
+            results = rag_chain.invoke({"input": question})
+            if results:
+                st.write((results['answer']))
+                st.write("Sources:")
+                for idx, item in enumerate(results['context']):
+                    st.write('\nSource %s:' % str(idx+1))
+                    st.write(results['context'][idx].metadata)
+                    st.write(results['context'][idx].page_content)
