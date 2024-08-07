b1ef738aae58fb06a58ae682fe8077d2a4817119
yanev-dev <127370286+yanev-dev@users.noreply.github.com>
2024-08-07 16:22:07 -0400

    spinners

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,44 +69,45 @@ def displayPDF(file):
 def main():
 
     def form_upload_callback():
-        if uploaded_docs is not None:
-            # save uploaded files to disk
-            for i in range(len(uploaded_docs)):
-                bytes_data = uploaded_docs[i].read()  # read the content of the file in binary
-                # print(uploaded_docs[i].name, bytes_data)
-                with open(os.path.join("/tmp", uploaded_docs[i].name), "wb") as f:
-                    f.write(bytes_data)  # write this content elsewhere
+        with st.spinner('Processing...'):
+            if uploaded_docs is not None:
+                # save uploaded files to disk
+                for i in range(len(uploaded_docs)):
+                    bytes_data = uploaded_docs[i].read()  # read the content of the file in binary
+                    # print(uploaded_docs[i].name, bytes_data)
+                    with open(os.path.join("/tmp", uploaded_docs[i].name), "wb") as f:
+                        f.write(bytes_data)  # write this content elsewhere
 
-            # parse the files and add to the splits collection
-            splits = []
-            for file in uploaded_docs:
-                with st.spinner('Loading...'):
-                    loader = PyPDFLoader(os.path.join("/tmp", file.name))
-                    docs = loader.load()
-                    # split the documents into chunks
-                    splits.extend(text_splitter.split_documents(docs))
+                # parse the files and add to the splits collection
+                splits = []
+                for file in uploaded_docs:
+                    with st.spinner('Loading...'):
+                        loader = PyPDFLoader(os.path.join("/tmp", file.name))
+                        docs = loader.load()
+                        # split the documents into chunks
+                        splits.extend(text_splitter.split_documents(docs))
 
-            if splits:
-                if 'vectorstore' not in st.session_state:
-                    # create the vectorestore to use as the index
-                    st.session_state['vectorstore'] = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-                    # expose this index in a retriever interface
-                    retriever = st.session_state['vectorstore'].as_retriever()
-                    # create a chain to answer questions 
-                    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-                    st.session_state['rag_chain'] = create_retrieval_chain(retriever, question_answer_chain)
+                if splits:
+                    if 'vectorstore' not in st.session_state:
+                        # create the vectorestore to use as the index
+                        st.session_state['vectorstore'] = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+                        # expose this index in a retriever interface
+                        retriever = st.session_state['vectorstore'].as_retriever()
+                        # create a chain to answer questions 
+                        question_answer_chain = create_stuff_documents_chain(llm, prompt)
+                        st.session_state['rag_chain'] = create_retrieval_chain(retriever, question_answer_chain)
 
     with st.form(key='uploader'):
         uploaded_docs = st.file_uploader('Upload trial documents in PDF format.',
                                  type=["pdf",],
                                  accept_multiple_files=True)
 
-        uploader_button = st.form_submit_button(label='Process Docs', on_click=form_upload_callback)
+        uploader_button = st.form_submit_button(label='Process Files', on_click=form_upload_callback)
 
     with st.form(key="questions"):
         question = st.write(
             "Now ask a question about the documents!")
-        question = st.text_input('Question to answer:')
+        question = st.text_input('Question:')
         retrieve = st.form_submit_button("Ask", type="primary")
         if retrieve and uploader_button:
             with st.spinner('Thinking...'):
