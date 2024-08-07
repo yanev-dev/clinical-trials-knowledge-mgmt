c67f6189b57c810795a0657fc5299438aa25c20c
yanev-dev <127370286+yanev-dev@users.noreply.github.com>
2024-08-07 16:06:14 -0400

    better state mgmt for chroma

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,10 +40,6 @@ text_splitter = SemanticChunker(
     OpenAIEmbeddings(), breakpoint_threshold_type="standard_deviation"
 )   
 
-uploaded_docs = st.file_uploader('Upload trial documents in PDF format.',
-                                 type=["pdf",],
-                                 accept_multiple_files=True)
-
 system_prompt = (
         """
         You are assisting a clinical research coordinator engaged in a clinical trial for a new drug.
@@ -63,8 +59,6 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-splits = []
-
 def displayPDF(file):
     with open(file, "rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
@@ -73,60 +67,66 @@ def displayPDF(file):
 
 
 def main():
-    if uploaded_docs is not None:
-        # save uploaded files
-        for i in range(len(uploaded_docs)):
-            bytes_data = uploaded_docs[i].read()  # read the content of the file in binary
-            # print(uploaded_docs[i].name, bytes_data)
-            with open(os.path.join("/tmp", uploaded_docs[i].name), "wb") as f:
-                f.write(bytes_data)  # write this content elsewhere
 
-        # parse the files and add to the splits collection
-        for file in uploaded_docs:
-            with st.spinner('Loading...'):
-                loader = PyPDFLoader(os.path.join("/tmp", file.name))
-                docs = loader.load()
-                # split the documents into chunks
-                splits.extend(text_splitter.split_documents(docs))
+    def form_upload_callback():
+        if uploaded_docs is not None:
+            # save uploaded files to disk
+            for i in range(len(uploaded_docs)):
+                bytes_data = uploaded_docs[i].read()  # read the content of the file in binary
+                # print(uploaded_docs[i].name, bytes_data)
+                with open(os.path.join("/tmp", uploaded_docs[i].name), "wb") as f:
+                    f.write(bytes_data)  # write this content elsewhere
 
-        if splits:
-            if 'vectorstore' not in st.session_state:
-                # create the vectorestore to use as the index
-                st.session_state['vectorstore'] = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-                # expose this index in a retriever interface
-                retriever = st.session_state['vectorstore'].as_retriever()
-                # create a chain to answer questions 
-                question_answer_chain = create_stuff_documents_chain(llm, prompt)
-                st.session_state['rag_chain'] = create_retrieval_chain(retriever, question_answer_chain)
+            # parse the files and add to the splits collection
+            splits = []
+            for file in uploaded_docs:
+                with st.spinner('Loading...'):
+                    loader = PyPDFLoader(os.path.join("/tmp", file.name))
+                    docs = loader.load()
+                    # split the documents into chunks
+                    splits.extend(text_splitter.split_documents(docs))
 
-            # ask the user for a question via `st.text_area`.
-            question = st.write(
-                "Now ask a question about the documents!")
+            if splits:
+                if 'vectorstore' not in st.session_state:
+                    # create the vectorestore to use as the index
+                    st.session_state['vectorstore'] = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+                    # expose this index in a retriever interface
+                    retriever = st.session_state['vectorstore'].as_retriever()
+                    # create a chain to answer questions 
+                    question_answer_chain = create_stuff_documents_chain(llm, prompt)
+                    st.session_state['rag_chain'] = create_retrieval_chain(retriever, question_answer_chain)
 
-            with st.form(key="questions"):
-                question = st.text_input('Question to answer:')
-                retrieve = st.form_submit_button("Ask", type="primary")
-                if retrieve:
-                    with st.spinner('Thinking...'):
-                        results = st.session_state['rag_chain'].invoke({"input": question})
-                        if results:
-                            st.write((results['answer']))
-                            st.write("Sources:")
-                            tabs_list = st.tabs(['\nSource %s:' % str(idx+1) for idx,_ in enumerate(results['context'])])                            
-                            for idx, item in enumerate(results['context']):
-                                with tabs_list[idx]:
-                                    st.write('File name: ' + results['context'][idx].metadata['source'])
-                                    page_num = int(results['context'][idx].metadata['page']) + 1
-                                    st.write('Page number: %d' % page_num)
-                                    pdf_viewer(results['context'][idx].metadata['source'],
-                                               width=900, 
-                                               height=1400, 
-                                               pages_to_render=[results['context'][idx].metadata['page']+1],
-                                               key='pdf'+str(idx))
-                                # st.write('\nSource %s:' % str(idx+1))
-                                # st.write(results['context'][idx].metadata)
-                                # st.write(results['context'][idx].page_content)
-                                # pdf_viewer(results['context'][idx].metadata['source'], pages_to_render=[results['context'][idx].metadata['page']+1], key='pdf'+str(idx))
+    with st.form(key='uploader'):
+        uploaded_docs = st.file_uploader('Upload trial documents in PDF format.',
+                                 type=["pdf",],
+                                 accept_multiple_files=True)
+
+        uploader_button = st.form_submit_button(label='Process Docs', on_click=form_upload_callback)
+
+    with st.form(key="questions"):
+        question = st.write(
+            "Now ask a question about the documents!")
+        question = st.text_input('Question to answer:')
+        retrieve = st.form_submit_button("Ask", type="primary")
+        if retrieve and uploader_button:
+            with st.spinner('Thinking...'):
+                results = st.session_state['rag_chain'].invoke({"input": question})
+                if results:
+                    st.write((results['answer']))
+                    st.write("Sources:")
+                    tabs_list = st.tabs(['\nSource %s:' % str(idx+1) for idx,_ in enumerate(results['context'])])                            
+                    for idx, item in enumerate(results['context']):
+                        with tabs_list[idx]:
+                            st.write('File name: ' + results['context'][idx].metadata['source'])
+                            page_num = int(results['context'][idx].metadata['page']) + 1
+                            st.write('Page number: %d' % page_num)
+                            pdf_viewer(results['context'][idx].metadata['source'],
+                                       width=900, 
+                                       height=1400, 
+                                       pages_to_render=[results['context'][idx].metadata['page']+1],
+                                       key='pdf'+str(idx))
+        else:
+            st.write("No results found: try a different question or upload different documents!")
 
 if __name__ == "__main__":
     main()
