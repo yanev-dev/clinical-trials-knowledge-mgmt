0cb5998cc3e57056a60afaee0de537a6bf5f7ff9
yanev-dev <127370286+yanev-dev@users.noreply.github.com>
2024-08-08 19:37:31 -0400

    page selector, no more main

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,82 +61,91 @@ prompt = ChatPromptTemplate.from_messages(
 )
 
 
-def main():
+if 'page_selection' not in st.session_state:
+    st.session_state['page_selection'] = []
 
-    def upload_callback():
-        with st.spinner('Processing...'):
-            # clean up the state
-            # eg del the vector store and qachain if already intialized with prior doc set
-            for key in st.session_state.keys():
-                del st.session_state[key]
+def upload_callback():
+    with st.spinner('Processing...'):
+        # clean up the state
+        # eg del the vector store and qachain if already intialized with prior doc set
+        for key in st.session_state.keys():
+            del st.session_state[key]
 
-            if uploaded_docs is not None:
-                # save uploaded files to disk
-                for i in range(len(uploaded_docs)):
-                    bytes_data = uploaded_docs[i].read()  # read the content of the file in binary
-                    # print(uploaded_docs[i].name, bytes_data)
-                    with open(os.path.join("/tmp", uploaded_docs[i].name), "wb") as f:
-                        f.write(bytes_data)  # write this content elsewhere
+        if uploaded_docs is not None:
+            # save uploaded files to disk
+            for i in range(len(uploaded_docs)):
+                bytes_data = uploaded_docs[i].read()  # read the content of the file in binary
+                # print(uploaded_docs[i].name, bytes_data)
+                with open(os.path.join("/tmp", uploaded_docs[i].name), "wb") as f:
+                    f.write(bytes_data)  # write this content elsewhere
 
-                # parse the files and add to the splits collection
-                splits = []
-                for file in uploaded_docs:
-                    with st.spinner('Loading...'):
-                        loader = PyPDFLoader(os.path.join("/tmp", file.name))
-                        docs = loader.load()
-                        # split the documents into chunks
-                        splits.extend(text_splitter.split_documents(docs))
+            # parse the files and add to the splits collection
+            splits = []
+            for file in uploaded_docs:
+                with st.spinner('Loading...'):
+                    loader = PyPDFLoader(os.path.join("/tmp", file.name))
+                    docs = loader.load()
+                    # split the documents into chunks
+                    splits.extend(text_splitter.split_documents(docs))
 
-                if splits:
-                    # create the vectorestore to use as the index
-                    st.session_state['vectorstore'] = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-                    # expose this index in a retriever interface
-                    retriever = st.session_state['vectorstore'].as_retriever()
-                    # create a chain to answer questions 
-                    question_answer_chain = create_stuff_documents_chain(llm, prompt)
-                    st.session_state['rag_chain'] = create_retrieval_chain(retriever, question_answer_chain)
+            if splits:
+                # create the vectorestore to use as the index
+                st.session_state['vectorstore'] = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+                # expose this index in a retriever interface
+                retriever = st.session_state['vectorstore'].as_retriever()
+                # create a chain to answer questions 
+                question_answer_chain = create_stuff_documents_chain(llm, prompt)
+                st.session_state['rag_chain'] = create_retrieval_chain(retriever, question_answer_chain)
 
-        st.write('Finished uploading...')
+    st.write('Finished uploading...')
 
-    def invoke_chain_callback():
-        with st.spinner('Thinking...'):
-            st.session_state['results'] = st.session_state['rag_chain'].invoke({"input": question})
-            st.write('Chain returned an answer...')
+def invoke_chain_callback():
+    with st.spinner('Thinking...'):
+        st.session_state['results'] = st.session_state['rag_chain'].invoke({"input": question})
+        st.write('Chain returned an answer...')
 
-    with st.form(key='uploader'):
-        uploaded_docs = st.file_uploader('Upload trial documents in PDF format.',
-                                 type=["pdf",],
-                                 accept_multiple_files=True)
+with st.form(key='uploader'):
+    uploaded_docs = st.file_uploader('Upload trial documents in PDF format.',
+                             type=["pdf",],
+                             accept_multiple_files=True)
 
-        uploader_button = st.form_submit_button(label='Process files', type="primary", on_click=upload_callback)
+    uploader_button = st.form_submit_button(label='Process files', type="primary", on_click=upload_callback)
 
-    # check if chain is ready before letting user ask questions 
-    asked = None   
-    if 'rag_chain' in st.session_state:
-        with st.form(key="questions"):
-            question = st.write("Now ask a question about the documents!")
-            question = st.text_input('Question:')
-            asked = st.form_submit_button("Ask", type="primary", on_click=invoke_chain_callback)                
-                    
-    if asked:
-        st.write(st.session_state['results']['answer'])
-        st.write("Sources:")
-        pages = []
-        source_list = ['\nSource %s:' % str(idx+1) for idx,_ in enumerate(st.session_state['results']['context'])]                            
-        for idx, item in enumerate(st.session_state['results']['context']):
-            st.write(source_list[idx])
-            st.write('File name: ' + st.session_state['results']['context'][idx].metadata['source'])
-            page_num = int(st.session_state['results']['context'][idx].metadata['page']) + 1
-            st.write('Page number: %d' % page_num)
-            pages.append(page_num)
-            
-        pdf_viewer(st.session_state['results']['context'][idx].metadata['source'],
-                   width=900, 
-                   height=1400, 
-                   pages_to_render=pages,
-                   key='pdf_'+str(idx))
-    # else:
-    #     st.write("No results found: try a different question or upload different documents!")
+# check if chain is ready before letting user ask questions 
+asked = None   
+if 'rag_chain' in st.session_state:
+    with st.form(key="questions"):
+        question = st.write("Now ask a question about the documents!")
+        question = st.text_input('Question:')
+        asked = st.form_submit_button("Ask", type="primary", on_click=invoke_chain_callback)                
+                
+if asked:
+    st.write(st.session_state['results']['answer'])
+    st.write("Sources:")
+    pages = []
+    source_list = ['\nSource %s:' % str(idx+1) for idx,_ in enumerate(st.session_state['results']['context'])]                            
+    for idx, item in enumerate(st.session_state['results']['context']):
+        st.write(source_list[idx])
+        st.write('File name: ' + st.session_state['results']['context'][idx].metadata['source'])
+        page_num = int(st.session_state['results']['context'][idx].metadata['page']) + 1
+        st.write('Page number: %d' % page_num)
+        pages.append(page_num)
 
-if __name__ == "__main__":
-    main()
+    with st.sidebar:
+    if not st.session_state['pages']:
+        st.session_state['page_selection'] = placeholder.multiselect(
+            "Select pages to display",
+            options=[],
+            default=[pages],
+            help="The page number considered is the PDF number and not the document page number.",
+            disabled=not st.session_state['pages'],
+            key='page_selector'
+        )
+        
+    pdf_viewer(st.session_state['results']['context'][idx].metadata['source'],
+               width=900, 
+               height=1400, 
+               pages_to_render=st.session_state['page_selection'],
+               key='pdf_'+str(idx))
+# else:
+#     st.write("No results found: try a different question or upload different documents!")
