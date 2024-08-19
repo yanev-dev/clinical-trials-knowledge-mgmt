4f728d65e5c899fdf11262d2d5d34dac9d12e0c8
yanev-dev <127370286+yanev-dev@users.noreply.github.com>
2024-08-19 14:15:50 -0400

    no rerun

--- streamlit_app.py
+++ streamlit_app.py
@@ -174,5 +174,4 @@ if 'rag_chain' in st.session_state:
         asked = st.form_submit_button("Ask", type="primary", on_click=invoke_chain_callback)
         if asked:
             st.write('Chain returned an answer...')
-            st.rerun()
             render_pdf_pages()
